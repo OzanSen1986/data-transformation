72ba8692dea1889f528db40075aa6bc0a8cbf206
Ozan Şen <ozanokan1903@gmail.com>
2025-09-26 18:59:59 +0300

    filter conditons by [year] and [genre] added.

--- dts_vgsales.py
+++ dts_vgsales.py
@@ -5,6 +5,11 @@ df = df.astype({'Year': 'Int32'})
 
 df.rename(columns={'EU_Sales':'SalesInEurope', 'JP_Sales': 'SalesInJapan'}, inplace = True)
 
+year_and_genre_filter = ((df['Year'] > 2016) | (df['Genre'] =='Shooter'))
+df = df.loc[year_and_genre_filter]
+
+print('----------------------------------->')
+
 print(f"Sales Amount of Europe : ${df['SalesInEurope'].sum():,.2f}")
 print(f"Sales Amount of Japan : ${df['SalesInJapan'].sum():,.2f}")
 print(f"Sales Amount of Global Sales: ${df['Global_Sales'].sum():,.2f}")
