ec891c055714fc99fa56716a52d8d20e0940919c
Ozan Şen <ozanokan1903@gmail.com>
2025-09-26 18:11:15 +0300

    Sales By Region has been added.

--- dts_vgsales.py
+++ dts_vgsales.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv('vgsales.csv')
+df = pd.read_csv('vgsales.csv', index_col='Rank')
+df = df.astype({'Year': 'Int32'})
 
-# short summary
-print(df.columns)
-print(f"Total number of sales rows: {df['EU_Sales'].count()}")
+df.rename(columns={'EU_Sales':'SalesInEurope', 'JP_Sales': 'SalesInJapan'}, inplace = True)
+
+print(f"Sales Amount of Europe : ${df['SalesInEurope'].sum():,.2f}")
+print(f"Sales Amount of Japan : ${df['SalesInJapan'].sum():,.2f}")
+print(f"Sales Amount of Global Sales: ${df['Global_Sales'].sum():,.2f}")
+print(f"Sales Amount of NA Sales: ${df['NA_Sales'].sum():,.2f}")
+print(f"Sales Amount of Other Sales: ${df['Other_Sales'].sum():,.2f}")
+
